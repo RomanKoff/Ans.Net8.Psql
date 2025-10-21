19e7ff148f822ab965a6832c7a31313c1adc5680
Roman Koff <krv@guap.ru>
2025-10-21 18:58:45 +0300

    2025-010-21

--- ~exts/DbContext.cs
+++ ~exts/DbContext.cs
@@ -80,20 +80,94 @@ namespace Ans.Net8.Psql
 		}
 
 
-		public static int Migration<T>(
-			this DbSet<T> dbSet,
-			IEnumerable<object> items,
-			string fieldsDefinitions)
-			where T : class
+		public static int Migration<TSource, TTarget>(
+			this DbSet<TTarget> target,
+			IEnumerable<TSource> source,
+			Func<TSource, TTarget> newItem,
+			int bufferCount = 999,
+			bool isQuiet = false)
+			where TSource : class
+			where TTarget : class
 		{
-			var (name1, context1) = dbSet.GetTableNameAndContext();
-			var helper1 = new MigrationTableHelper(name1, fieldsDefinitions);
-			foreach (var item1 in items)
-				helper1.AddInsert(item1);
-			var count1 = context1.ExecuteSqlRawIfPresent(helper1.Sql.ToString());
-			_ = context1.SerialSequenceSetMax(name1);
+			if (!(source?.Count() > 0))
+				return 0;
+			var (name1, db1) = target.GetTableNameAndContext();
+			var count1 = 0;
+			var c1 = source.Count();
+			var i1 = bufferCount;
+			if (!isQuiet)
+			{
+				Console.Write($"{name1}: ");
+				SuppConsole.CursorSavePos();
+				Console.CursorVisible = false;
+			}
+			foreach (var item1 in source)
+			{
+				target.Add(newItem(item1));
+				if (--i1 < 1)
+				{
+					count1 += db1.SaveChanges();
+					i1 = bufferCount;
+				}
+				if (!isQuiet)
+				{
+					Console.Write($"{c1--}   ");
+					SuppConsole.CursorRestopePos();
+				}
+			}
+			count1 += db1.SaveChanges();
+			db1.SerialSequenceSetMax($"{name1}");
+			if (!isQuiet)
+			{
+				Console.WriteLine(SuppLangEn.GetDeclineEn(
+				"{0} {1} added.",
+				target.Count(),
+				"entity",
+				"entites"));
+				Console.CursorVisible = true;
+				Console.WriteLine();
+			}
 			return count1;
 		}
+
+
+		public static int MigrationIfNotAny<TSource, TTarget>(
+			this DbSet<TTarget> target,
+			IEnumerable<TSource> source,
+			Func<TSource, TTarget> newItem,
+			int bufferCount = 500)
+			where TSource : class
+			where TTarget : class
+		{
+			if (target.Any())
+			{
+				var (name1, _) = target.GetTableNameAndContext();
+				Console.Write($"{name1}: ");
+				Console.WriteLine(SuppLangEn.GetDeclineEn(
+					"contains {0} {1}.",
+					target.Count(),
+					"entity",
+					"entites"));
+				return 0;
+			}
+			return target.Migration(source, newItem, bufferCount);
+		}
+
+
+		//public static int Migration<T>(
+		//	this DbSet<T> dbSet,
+		//	IEnumerable<object> items,
+		//	string fieldsDefinitions)
+		//	where T : class
+		//{
+		//	var (name1, context1) = dbSet.GetTableNameAndContext();
+		//	var helper1 = new MigrationTableHelper(name1, fieldsDefinitions);
+		//	foreach (var item1 in items)
+		//		helper1.AddInsert(item1);
+		//	var count1 = context1.ExecuteSqlRawIfPresent(helper1.Sql.ToString());
+		//	_ = context1.SerialSequenceSetMax(name1);
+		//	return count1;
+		//}
 
 
 		public static int ExecuteSqlRawIfPresent(
